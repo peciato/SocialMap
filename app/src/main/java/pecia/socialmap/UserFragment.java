513d92d660a96916a72f16c2702e1f008c7df349
peciato <peciato1992@gmail.com>
2017-06-07 22:05:30 +0200

    ultima funzionante

---
+++ app/src/main/java/pecia/socialmap/UserFragment.java
@@ -0,0 +1,24 @@
+package pecia.socialmap;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Pierluca on 5/31/2017.
+ */
+
+public class UserFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.first_layout, container, false);
+        return myView;
+    }
+}
